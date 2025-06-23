04b8bd9bfd32d078da0704a18c38b83b840883ed
Ian Handy <ianhandy@Ians-MacBook-Pro.local>
2025-06-23 11:48:40 -0400

    Fixed append_coord to match naming of sit col

--- python_utils/data_utils.py
+++ python_utils/data_utils.py
@@ -71,18 +71,35 @@ def calculate_correlation(df, columns=None):
 # Add more utility functions here as needed
 
 def append_coord(df):
-    coordinates = {
-        'Leland Farm' : [42.063835,-71.249616],
-        'Sachem Rock' : [42.018333,-70.951667],
-        'Dunrovin Farm' : [42.003699,-70.840169],
-        'Christos' : [42.06734,-71.00287],
-        'Native Meadow' : [42.09107339,-71.04386531],
-        'SoutheasternVocTech' : [42.183781,-71.101059],
-        'Easton Powerline' : [42.183781,-71.101059],
-        'Stonehill Farm' : [42.183781,-71.101059],
-        'VA Hospital' : [42.183781,-71.101059],
-        'Beaver Brook' : [42.183781,-71.101059],
-        }
-    df['lat'] = df['sample_site'].apply(lambda x: coordinates[x][0] if x in coordinates else None)
-    df['lon'] = df['sample_site'].apply(lambda x: coordinates[x][1] if x in coordinates else None)
+    site_column = next((col for col in df.columns if 'site' in col.lower()), None)
+    if site_column is None:
+        raise ValueError("No column containing 'site' found in DataFrame.")
+
+    else:
+        coordinates = {
+            'Leland Farm' : [42.063835,-71.249616],
+            'Sachem Rock' : [42.018333,-70.951667],
+            'Dunrovin Farm' : [42.003699,-70.840169],
+            'Christos' : [42.06734,-71.00287],
+            'Native Meadow' : [42.09107339,-71.04386531],
+            'SoutheasternVocTech' : [42.183781,-71.101059],
+            'Easton Powerline' : [42.183781,-71.101059],
+            'Stonehill Farm' : [42.183781,-71.101059],
+            'VA Hospital' : [42.183781,-71.101059],
+            'Beaver Brook' : [42.183781,-71.101059],
+            #Temporary fix for different site names with and without spaces
+            'LelandFarm' : [42.063835,-71.249616],
+            'SachemRock' : [42.018333,-70.951667],
+            'DunrovinFarm' : [42.003699,-70.840169],
+            'Christos' : [42.06734,-71.00287],
+            'NativeMeadow' : [42.09107339,-71.04386531],
+            'SoutheasternVocTech' : [42.183781,-71.101059],
+            'EastonPowerline' : [42.183781,-71.101059],
+            'StonehillFarm' : [42.183781,-71.101059],
+            'VAHospital' : [42.183781,-71.101059],
+            'BeaverBrook' : [42.183781,-71.101059],
+
+            }
+        df['lat'] = df[site_column].apply(lambda x: coordinates[x][0] if x in coordinates else None)
+        df['lon'] = df[site_column].apply(lambda x: coordinates[x][1] if x in coordinates else None)
     return df
